6325b1373a3b2a28d3c90f7b7b7fdfc7acb83e1e
Edmund To <yatlunto@gmail.com>
2017-05-23 19:58:12 +0800

    completed cross validation for binary-classifiers

---
+++ foundation/5-cross-validation/script.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+admissions = pd.read_csv("admissions.csv")
+admissions["actual_label"] = admissions["admit"]
+admissions = admissions.drop("admit", axis=1)
+
+print(admissions.head())
+
+print('holdout validation') 
+
+import numpy as np
+np.random.seed(8)
+
+shuffled_index = np.random.permutation(admissions.index)
+shuffled_admissions = admissions.loc[shuffled_index]
+train = shuffled_admissions.iloc[0:515]
+test = shuffled_admissions.iloc[515:len(shuffled_admissions)]
+model = LogisticRegression()
+model.fit(train[["gpa"]], train["actual_label"])
+
+labels = model.predict(test[["gpa"]])
+test["predicted_label"] = labels
+
+matches = test["predicted_label"] == test["actual_label"]
+correct_predictions = test[matches]
+accuracy = float(len(correct_predictions)) / len(test)
+print('accuracy here: correct predict/total')
+print(accuracy)
+
+
+model = LogisticRegression()
+model.fit(train[["gpa"]], train["actual_label"])
+labels = model.predict(test[["gpa"]])
+test["predicted_label"] = labels
+matches = test["predicted_label"] == test["actual_label"]
+correct_predictions = test[matches]
+accuracy = len(correct_predictions) / len(test)
+true_positive_filter = (test["predicted_label"] == 1) & (test["actual_label"] == 1)
+true_positives = len(test[true_positive_filter])
+false_negative_filter = (test["predicted_label"] == 0) & (test["actual_label"] == 1)
+false_negatives = len(test[false_negative_filter])
+
+sensitivity = float(true_positives) / (true_positives + false_negatives)
+
+print('sensitivity')
+print(sensitivity)
+
+false_positive_filter = (test["predicted_label"] == 1) & (test["actual_label"] == 0)
+false_positives = len(test[false_positive_filter])
+true_negative_filter = (test["predicted_label"] == 0) & (test["actual_label"] == 0)
+true_negatives = len(test[true_negative_filter])
+
+specificity = float(true_negatives) / (false_positives + true_negatives)
+print('specificity')
+print(specificity)
+
+
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+probabilities = model.predict_proba(test[["gpa"]])
+fpr, tpr, thresholds = metrics.roc_curve(test["actual_label"], probabilities[:,1])
+plt.plot(fpr, tpr)
+plt.show()
+
+
+
+# Note the different import style!
+from sklearn.metrics import roc_auc_score
+probabilities = model.predict_proba(test[["gpa"]])
+
+# Means we can just use roc_auc_curve() instead of metrics.roc_auc_curve()
+auc_score = roc_auc_score(test["actual_label"], probabilities[:,1])
+print('This is the area under curve ')
+print(auc_score)
